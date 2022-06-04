f91eeac3713f52ed7e23ea821106041f5aff6f7f
Ahmad Raza <ahmadraza4119@gmail.com>
2022-06-04 18:18:22 +0500

    GC-3/improve search bar

--- themes/homey-child/js/gida-custom.js
+++ themes/homey-child/js/gida-custom.js
@@ -3,11 +3,11 @@ jQuery(document).ready(function() {
         jQuery('.airbnb-container').hide(500);
         jQuery('.search-fields').show(500).animate({height: "70px", width: "800px"});
         jQuery('.search-fields .search-destination button').addClass('airbnb-active');
-        jQuery('.search-fields .search-destination button span.filter-option').addClass('text-bold ').css({"left": "20px"});
+        jQuery('.search-fields .search-destination button span.filter-option').addClass('text-bold ').css({"left": "8px", "top": "24px"});
         jQuery('.search-fields .search-destination button').click();
     });
 
-    jQuery('.search-fields .search-destination button').click(function() {
+    jQuery('.search-fields .search-destination button').on('focus', function() {
         jQuery('.search-fields, .search-fields .search-destination button, .search-fields .search-date-range input, .search-fields .search-guests input').css('background-color', 'rgb(246, 246, 246)');
 
         jQuery('.search-fields .search-guests input').removeClass('airbnb-active').parent().removeClass('z-index');
@@ -16,7 +16,26 @@ jQuery(document).ready(function() {
         jQuery(this).addClass("airbnb-active");
     });
 
-    jQuery(".main-search-date-range-js input").on('focus', function() {
+    jQuery('.search-fields .search-destination select[name=city]').change(function() {
+        jQuery(".search-fields .main-search-date-range-js .search-date-range-arrive input").focus();
+    });
+
+    // add guests
+    jQuery('#airbnb-addguest').on('click', function() {
+        jQuery('.search-fields, .search-fields .search-destination button, .search-fields .search-date-range input, .search-fields .search-guests input').css('background-color', 'rgb(246, 246, 246)');
+
+        jQuery('.airbnb-container').hide(500);
+        jQuery('.search-fields').show(500).animate({height: "70px", width: "800px"});
+        jQuery('.search-fields .search-guests input').click();
+    });
+
+    jQuery('.search-fields .search-guests input').click(function() {
+        jQuery(this).focus();
+        jQuery('.search-fields .search-guests-wrap').css('display', 'block');
+    });
+    //---------------------
+
+    jQuery(".search-fields .main-search-date-range-js input").on('focus', function() {
         jQuery('.search-fields .search-guests input').removeClass('airbnb-active').parent().removeClass('z-index');
         jQuery('.search-fields .search-destination button').removeClass('airbnb-active');
         focusedInput = jQuery(this).attr('name');
